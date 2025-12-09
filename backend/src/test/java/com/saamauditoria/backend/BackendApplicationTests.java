17e54d13b9d21384c066e7212a1c074842aee664
Luis Eduardo <luis.marcato2000@gmail.com>
2025-12-09 20:40:36 -0300

    feat: initialize Spring Boot project with basic dependencies

---
+++ backend/src/test/java/com/saamauditoria/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saamauditoria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
