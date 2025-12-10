f3a3480967bf328dae7ceb98cae2e3526a3ca031
Luis Eduardo <luis.marcato2000@gmail.com>
2025-12-09 21:01:21 -0300

    feat: add migration V2 - seed initial data

---
+++ backend/src/main/resources/db/migration/V2__seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_users (email, password, role)
+VALUES (
+    'admin@saamauditoria.com',
+    '$2a$10$XH/Ik.I0qxLpJ02qJJov6OaIYu1cj0gQT5xW15.RDMQo0ycq8on5e',
+    'ADMIN'
+);
+
+INSERT INTO tb_employees (name, admission_date, salary, status)
+VALUES (
+    'Luis Eduardo Marcato',
+    '2025-12-09',
+    1800.00,
+    'ATIVO'
+);
